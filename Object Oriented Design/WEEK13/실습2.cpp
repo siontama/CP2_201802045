ebd7bf8d82a99d1adb8ced3d26ca3f28316ae8de
Yami <31057849+siontama@users.noreply.github.com>
2020-11-29 03:51:47 +0900

    WEEK13

---
+++ Object Oriented Design/WEEK13/실습2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+template <typename T>
+auto sum(T x) {
+	return x;
+}
+template<typename T, typename...Args>
+auto sum(T x, Args... args) {
+	return x + sum(args...);
+}
+int main() {
+	auto x = sum(42.5, 10, 11.1f);
+	std::cout << x;
+	getchar();
+}
