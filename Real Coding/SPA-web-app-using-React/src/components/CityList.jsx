51990a2ef58dbb7a950085d93fbf2151d6983ae1
siontama <siontama@naver.com>
2021-05-19 18:37:25 +0900

    feapp-weather

---
+++ Real Coding/SPA-web-app-using-React/src/components/CityList.jsx
@@ -0,0 +1,15 @@
+function CityList(props) {
+    // const cities = props.cities;
+    const { cities } = props;
+    return (
+      <div>
+        <h1>CityList</h1>
+        <ul className="citylist">
+            {cities.map((item, index) => {
+                return <li key={index}>{item}</li>;
+            })}
+        </ul>
+      </div>
+    );
+  }
+  export default CityList;
